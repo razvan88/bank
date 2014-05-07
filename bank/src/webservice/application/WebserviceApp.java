49f6275a457bad518a651927f9ae4fada6d0bc66
Razvan Nedelcu <razvannedelcu2004@yahoo.com>
2014-05-07 17:11:22 +0300

    very first skeleton

---
+++ bank/src/webservice/application/WebserviceApp.java
@@ -0,0 +1,46 @@
+package webservice.application;
+
+import org.restlet.*;
+import org.restlet.data.Protocol;
+
+/**
+ * This class starts and stops the webservice
+ * 
+ * @author Razvan Nedelcu
+ */
+public class WebserviceApp {
+	private static Component component;
+	private static WebserviceApp webservice;
+	
+	static {
+		component = new Component();
+		webservice = new WebserviceApp();
+	}
+	
+	private WebserviceApp() { }
+	
+	public static WebserviceApp getInstance() {
+		return webservice;
+	}
+	
+	public void startWebservice() throws Exception {
+		if(component.isStarted())
+			return;
+		
+		int httpPort = 31889;
+		final String maxThreadsKey = "maxThreads";
+		final String maxThreadsVal = "512";
+		
+		Server server = component.getServers().add(Protocol.HTTP, httpPort);  
+		server.getContext().getParameters().add(maxThreadsKey, maxThreadsVal); 
+		component.getDefaultHost().attach(new WebserviceDispatcher());  
+		component.start();
+	}
+	
+	public void stopWebservice() throws Exception {
+		if (component.isStopped())
+			return;
+		
+		component.stop();
+	}
+}
---
+++ bank/src/webservice/application/WebserviceDispatcher.java
@@ -0,0 +1,25 @@
+package webservice.application;
+
+
+import org.restlet.*;
+import org.restlet.routing.Router;
+
+
+/**
+ * Used to create a root restlet that will receive all the
+ * incoming requests
+ * 
+ * @author Razvan Nedelcu
+ */
+public class WebserviceDispatcher extends Application {
+	
+	@Override
+	public synchronized Restlet createInboundRoot() {
+		Router router = new Router(getContext());
+		
+		//router.attach("/getSchoolsList", SchoolsListResource.class);
+		
+		
+		return router;
+	}
+}
