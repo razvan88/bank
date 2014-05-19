b8b7d1bd1e8d31af73e10452106b0584ea526c3e
Razvan Nedelcu <razvannedelcu2004@yahoo.com>
2014-05-19 13:27:50 +0300

    close webserver if crashes

--- bank/src/main/MainClass.java
+++ bank/src/main/MainClass.java
@@ -2,19 +2,22 @@ package main;
 
 import webservice.application.WebserviceApp;
 
-
 /**
  * The main class, the entry point of the Server application
  */
 public class MainClass {
 	public static void main(String[] args) {
 		WebserviceApp webservice = WebserviceApp.getInstance();
-		
+
 		try {
 			webservice.startWebservice();
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			try {
+				webservice.stopWebservice();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 		}
-		
+
 	}
 }
